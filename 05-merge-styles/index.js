9cec7e3a5bfd1244ad918f9430f2fd455310a3e5
idiamondcat <shock_harrison@yahoo.com>
2023-05-09 05:53:10 +0700

    fix task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
+const fsP = require('fs/promises');
 const path = require('path');
 
-fs.readdir(path.resolve(__dirname, 'styles'), {withFileTypes:true}, (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        let {name} = file;
-        let ext = path.extname(name);
-        if (ext === '.css') {
-            fs.readFile(path.resolve(__dirname, 'styles', name), 'utf-8', (err, data) => {
-                if (err) throw err;
-                fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, err => {
-                    if (err) throw err;
-                })
+const writableStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+const mergeStyles = async function() {
+    const allFiles = await fsP.readdir(path.resolve(__dirname, 'styles'));
+    allFiles.forEach(file => {
+        try {
+            fs.stat(path.resolve(__dirname, 'styles', file), (err, stats) => {
+                if (stats.isFile() && path.extname(file) === '.css') {
+                    fs.createReadStream(path.resolve(__dirname, 'styles', file), 'utf-8');
+                    fs.readFile(path.resolve(__dirname, 'styles', file), 'utf-8', (err, data) => {
+                        if (err) throw err;
+                        fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, err => {
+                            if (err) throw err;
+                        })
+                    })
+                }
             })
+        } catch (err) {
+            console.log(err);
         }
     })
-})
+}
+mergeStyles();

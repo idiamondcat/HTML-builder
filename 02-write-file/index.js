b16746afa3142bcd45f9e16f67dca9a06c6fa404
idiamondcat <shock_harrison@yahoo.com>
2023-05-10 19:01:47 +0700

    fix task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const {stdin: input, stdout: output} = require('process');
+const {stdin: input, stdout: output, exit: exit} = require('process');
 const rl = readline.createInterface({input, output});
+const writableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 const writeData = async (path) => {
-    console.log('Hello, type something...\n');
+    output.write('Hello, type something...\n');
     rl.on('line', (input) => {
-        if (input === 'exit') {
+        if (input.toLowerCase() === 'exit') {
             input = '';
-            console.log('Goodbye');
+            output.write('Goodbye');
             rl.close();
+            exit();
         }
-        fs.appendFile(path, input + '\n', (err) => {
+        writableStream.write(input + '\n', (err) => {
             if (err) throw err;
         })
     })
     rl.on('SIGINT', () => {
-        console.log('Goodbye');
+        output.write('Goodbye');
         rl.close();
+        exit();
     })
 };
 writeData(path.join(__dirname, 'text.txt'));
